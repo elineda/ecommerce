221ba3950079cc13f45b2e3af8e50f9109f7f640
MarineAllione <marineallione4243@gmail.com>
2018-11-19 10:12:30 +0100

    rajouter lien pour boostrap + le script

--- index.php
+++ index.php
@@ -6,13 +6,14 @@
     <title>Design Tech Académie</title>
     <link rel="stylesheet" href="dom.css" />
     <link rel="stylesheet" href="http://51.75.126.53/jquery/jquery-ui.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
     <header>
     </header>
     <section>
     </section>
-
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="http://51.75.126.53/jquery/jquery-ui.js"></script>
     <script src="script.js" type="text/javascript"></script>
