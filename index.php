862be7a1f13f1b44981ac1bc6ce60e64f9c06db6
Elineda <henecro@yahoo.fr>
2018-11-19 13:45:36 +0100

    blabla

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
   <head>
     <meta charset="utf-8">
     <title>Design Tech Académie</title>
-    <link rel="stylesheet" href="http://51.75.126.53/jquery/jquery-ui.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="style1.css" />
+    <link rel="stylesheet" href="style.css" />
   </head>
   <body>
     <header>
