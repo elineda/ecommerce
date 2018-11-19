751e6abf6ffa3ce2e8bcd16a2024744d9df4da2d
Elineda <henecro@yahoo.fr>
2018-11-19 09:44:12 +0100

    first

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Design Tech Académie</title>
+    <link rel="stylesheet" href="dom.css" />
+    <link rel="stylesheet" href="http://51.75.126.53/jquery/jquery-ui.css" />
+  </head>
+  <body>
+    <header>
+    </header>
+    <section>
+    </section>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="http://51.75.126.53/jquery/jquery-ui.js"></script>
+    <script src="script.js" type="text/javascript"></script>
+  </body>
+</html>
